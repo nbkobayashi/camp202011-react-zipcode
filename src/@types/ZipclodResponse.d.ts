b5185a753ccd06c219e1932c5fae68cb589ad95f
nbkobayashi <73633610+nbkobayashi@users.noreply.github.com>
2020-12-04 11:14:18 +0900

    Add search functions

---
+++ src/@types/ZipclodResponse.d.ts
@@ -0,0 +1,16 @@
+interface ZipcloudResponse {
+  message: string;
+  results: ZipcloudResponseResult[];
+  status: number;
+}
+
+interface ZipcloudResponseResult {
+  address1: string;
+  address2: string;
+  address3: string;
+  kana1: string;
+  kana2: string;
+  kana3: string;
+  prefcode: string;
+  zipcode: string;
+}
